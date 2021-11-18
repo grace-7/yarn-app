ba0d53fe30f0b611334ce96e0b2443ef966bb003
grace-7 <89417667+grace-7@users.noreply.github.com>
2021-11-18 12:15:36 -0500

    Update PatternTranslator.vue

--- src/components/PatternTranslator.vue
+++ src/components/PatternTranslator.vue
@@ -2,7 +2,7 @@
   <form class="input"> 
     <label for="newPattern"></label>
     <span>Please copy and paste your pattern below:</span>
-    <textarea id="input" v-model="message" placeholder="add multiple lines"></textarea>
+    <textarea rows="10" id="input" v-model="message" placeholder="add multiple lines"></textarea>
   </form>
   <button @click="convert()" id="botConvert" class="btn">
     Convert
@@ -26,33 +26,62 @@ setup(props, ctx) {
   let message = ref("");
   let outputtedPattern = ref("");
 
-  function convert(){
-
-
-    for (let i = 0; i < message.value.length; i++){
-
-    let splitAtRow = message.value.split("Row" || "Round");
-
-
-    }
-
-    
-      // ch = 'chain' 
-      // sc = 'single crochet'
-      // dc = 'double crochet'
-      // tr = 'treble crochet'
-      // ss = 'slip stitch'
-      // sp = 'space'
+  function convert() {
+    outputtedPattern.value = translatePattern(message.value);
       
   }
 
-  function loopThroughLine(){
+  function translatePattern(pattern) {
 
+  }
+
+  function translateLine(line) {
+    console.log("here loop through line");
+    for (let i = 0; i < message.value.length; i++){
+
+      let splitAtRow = message.value.split("Row" || "Round");
+      outputtedPattern.value = splitAtRow;
+
+
+    }
+  }
+
+  function translateInstruction(instruction) {
+    console.log("here loop through instruction");
     let splitAtComma = message.value.split(",");
 
     for (let i = 0; i < splitAtComma.length; i++){
       console.log("here");
       outputtedPattern.value = converter.toWords(message.value);
+    }
+  }
+
+  function translateToken(token) {
+    console.log("here loop through token");
+    for (let i = 0; i < message.value.length; i++) {
+      let instruction = document.getElementById("input").value;
+      if (instruction === "ch") {
+        instruction === "chain"
+      }
+      if (instruction === "sc") {
+        instruction === "single crochet"
+      }
+      if (instruction === "dc") {
+        instruction === "single crochet"
+      }
+      if (instruction === "sc") {
+        instruction === "double crochet"
+      }
+      if (instruction === "trc") {
+        instruction === "treble crochet"
+      }
+      if (instruction === "ss") {
+        instruction === "slip stitch"
+      }
+      if (instruction === "sp") {
+        instruction === "space"
+      }
+      outputtedPattern = instruction;
     }
   }
 
@@ -69,27 +98,6 @@ setup(props, ctx) {
   }
 
 }
-
-//   name: 'saveButton',
-//   props: {
-//     text: String
-//   },
-//   data: function () {
-//     return {
-//       outputtedPattern: "",
-//       message: ""
-//     }
-//   },
-//   methods: {
-//     convert(){
-      
-//     },
-
-//     saveButton() {
-//       // this.$emit('btn-click')
-//       console.log(this.message)
-//     },
-//   },
 }
 
 
